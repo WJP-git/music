c222d7e4644483243f939f74a905898283211b1b
Jonathan_Lee <lnj@it666.com>
2019-12-08 16:29:59 +0800

    首页-颜色换肤

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
   <div id="app">
-    <div class="test"></div>
-    <div>我是段落</div>
+    <Header></Header>
+    <div class="theme">
+      <button @click="myFn('theme')"></button>
+      <button @click="myFn('theme1')"></button>
+      <button @click="myFn('theme2')"></button>
+    </div>
   </div>
 </template>
-
-<style lang="scss">
-  .test{
-    width: 100px;
-    height: 100px;
-    background: #f00;
-    /*transform: translate(100px, 100px);*/
+<script>
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  methods: {
+    myFn (data) {
+      // console.log(data)
+      document.documentElement.setAttribute('data-theme', data)
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+  @import "assets/css/variable";
+  .theme{
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    button{
+      width: 100px;
+      height: 100px;
+      &:nth-of-type(1){
+        background: $background-color-theme;
+      }
+      &:nth-of-type(2){
+        background: $background-color-theme1;
+      }
+      &:nth-of-type(3){
+        background: $background-color-theme2;
+      }
+    }
   }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header">
+      <div class="header-left"></div>
+      <p class="header-title">知播渔音乐</p>
+      <div class="header-right"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../assets/css/variable";
+  @import "../assets/css/mixin";
+.header{
+  width: 100%;
+  height: 100px;
+  /*background: #f00;*/
+  @include bg_color($background-color-theme);
+  display: flex;
+  justify-content: space-between;
+  .header-left, .header-right{
+    width: 84px;
+    height: 84px;
+    background: #000;
+    margin-top: 8px;
+  }
+  .header-title{
+    text-align: center;
+    line-height: 100px;
+  }
+}
+</style>
