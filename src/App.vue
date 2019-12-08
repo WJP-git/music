6d366167dd45dc6bef8034f8998c33c1faa11237
Jonathan_Lee <lnj@it666.com>
2019-12-08 16:02:05 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div class="test"></div>
+    <div>我是段落</div>
+  </div>
+</template>
+
+<style lang="scss">
+  .test{
+    width: 100px;
+    height: 100px;
+    background: #f00;
+    /*transform: translate(100px, 100px);*/
+  }
+</style>
