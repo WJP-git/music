1ffd7cb8a9bef8a5d2317c1ba555e912dde0a61d
Jonathan_Lee <lnj@it666.com>
2019-12-09 19:22:41 +0800

    推荐界面-最新音乐

---
+++ src/components/SongList.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="song">
+      <div class="song-top">
+        <h3>最新音乐</h3>
+      </div>
+      <ul class="song-list">
+        <li v-for="value in songs" :key="value.id" class="item">
+          <img :src="value.song.album.picUrl" alt="">
+          <div>
+            <h3>{{value.name}}</h3>
+            <p>{{value.song.artists[0].name}}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SongList',
+  props: {
+    songs: {
+      type: Array,
+      default: () => [],
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../assets/css/mixin";
+  @import "../assets/css/variable";
+.song{
+  width: 100%;
+  @include bg_sub_color();
+  .song-top{
+    width: 100%;
+    height: 84px;
+    line-height: 84px;
+    @include bg_sub_color();
+    border-bottom: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    padding: 0 20px;
+    h3{
+      @include font_size($font_large);
+      font-weight: bold;
+      @include font_color();
+    }
+  }
+  .song-list{
+    width: 100%;
+    overflow: hidden;
+    .item{
+      width: 100%;
+      height: 150px;
+      display: flex;
+      align-items: center;
+      padding: 0 20px;
+      margin-bottom: 20px;
+      border-bottom: 1px solid #ccc;
+      img{
+        width: 100px;
+        height: 100px;
+        border-radius: 20px;
+        margin-right: 20px;
+      }
+      div{
+        h3{
+          @include font_size($font_large);
+          @include font_color();
+        }
+        p{
+          @include font_size($font_samll);
+          @include font_color();
+          opacity: 0.6;
+          margin-top: 20px;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -3,25 +3,29 @@
       <Banner :banners="banners"></Banner>
       <Personalized :personalized="personalized" :title="'推荐歌单'"></Personalized>
       <Personalized :personalized="albums" :title="'最新专辑'"></Personalized>
+      <SongList :songs="songs"></SongList>
     </div>
 </template>
 
 <script>
-import { getBanner, getPersonalized, getNewAlbum } from '../api/index'
+import { getBanner, getPersonalized, getNewAlbum, getNewSong } from '../api/index'
 import Banner from '../components/Banner'
 import Personalized from '../components/Personalized'
+import SongList from '../components/SongList'
 
 export default {
   name: 'Recommend',
   components: {
     Banner,
-    Personalized
+    Personalized,
+    SongList
   },
   data () {
     return {
       banners: [],
       personalized: [],
-      albums: []
+      albums: [],
+      songs: []
     }
   },
   created () {
@@ -45,6 +49,14 @@ export default {
       .then((data) => {
         // console.log(data.albums.splice(0, 6))
         this.albums = data.albums.splice(0, 6)
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+    getNewSong()
+      .then((data) => {
+        // console.log(data)
+        this.songs = data.result
       })
       .catch(function (err) {
         console.log(err)
