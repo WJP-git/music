e102fce6d2065d3d4affaf9a21c7fb13a29c4e49
Jonathan_Lee <lnj@it666.com>
2019-12-09 14:53:10 +0800

    首页-路由切换界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 注意点: 以下的导入方式, 无论组件有没有被用到都会被加载进来
+import Recommend from '../views/Recommend'
+import Singer from '../views/Singer'
+import Rank from '../views/Rank'
+import Search from '../views/Search'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/recommend' },
+  { path: '/recommend', component: Recommend },
+  { path: '/singer', component: Singer },
+  { path: '/rank', component: Rank },
+  { path: '/search', component: Search }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Recommend.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="recommend">
+      热门推荐
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Recommend'
+}
+</script>
+
+<style scoped>
+
+</style>
