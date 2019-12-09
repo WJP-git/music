afbb843f9a736b9999da6da8ae95a773deb6a44d
Jonathan_Lee <lnj@it666.com>
2019-12-09 15:16:04 +0800

    首页-组件按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// 注意点: 以下的导入方式, 无论组件有没有被用到都会被加载进来
-import Recommend from '../views/Recommend'
-import Singer from '../views/Singer'
-import Rank from '../views/Rank'
-import Search from '../views/Search'
+// 注意点: 通过 improt xxx from xxx的方式加载组件, 无论组件有没有被用到, 都会被加载进来
+// import Recommend from '../views/Recommend'
+// import Singer from '../views/Singer'
+// import Rank from '../views/Rank'
+// import Search from '../views/Search'
+
+// 实现Vue组件的按需加载
+const Recommend = (resolve) => {
+  import('../views/Recommend').then((module) => {
+    resolve(module)
+  })
+}
+const Singer = (resolve) => {
+  import('../views/Singer').then((module) => {
+    resolve(module)
+  })
+}
+const Rank = (resolve) => {
+  import('../views/Rank').then((module) => {
+    resolve(module)
+  })
+}
+const Search = (resolve) => {
+  import('../views/Search').then((module) => {
+    resolve(module)
+  })
+}
 
 Vue.use(VueRouter)
 
