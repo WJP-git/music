01494e95e8baf3437ab21a7ed1ec39ba426102d1
Jonathan_Lee <lnj@it666.com>
2019-12-12 17:59:54 +0800

    推荐界面-歌单详情过渡动画

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -8,7 +8,9 @@
         <SongList :songs="songs"></SongList>
       </div>
     </ScrollView>
-    <router-view></router-view>
+    <transition>
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -87,5 +89,24 @@ export default {
   right: 0;
   bottom: 0;
   overflow: hidden;
+}
+
+.v-enter{
+  transform: translateX(100%);
+}
+.v-enter-to{
+  transform: translateX(0%);
+}
+.v-enter-active{
+  transition: transform 1s;
+}
+.v-leave{
+  transform: translateX(0%);
+}
+.v-leave-to{
+  transform: translateX(100%);
+}
+.v-leave-active{
+  transition: transform 1s;
 }
 </style>
