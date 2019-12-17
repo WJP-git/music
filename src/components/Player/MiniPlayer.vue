7b92fa4928a5dbcf26c75bb2396082dfb77fee33
Jonathan_Lee <lnj@it666.com>
2019-12-17 18:53:48 +0800

    播放界面-迷你播放器

---
+++ src/components/Player/MiniPlayer.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="mini-player">
+      <div class="player-warpper">
+        <div class="player-left">
+          <img src="https://y.gtimg.cn/music/photo_new/T002R300x300M000003y8dsH2wBHlo.jpg" alt="">
+          <div class="player-title">
+            <h3>演员</h3>
+            <p>薛之谦</p>
+          </div>
+        </div>
+        <div class="player-right">
+          <div class="play"></div>
+          <div class="list"></div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MiniPlayer'
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../../assets/css/variable";
+  @import "../../assets/css/mixin";
+.mini-player{
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 150px;
+  .player-warpper{
+    width: 100%;
+    height: 100%;
+    @include bg_color();
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .player-left{
+      display: flex;
+      padding-left: 30px;
+      img{
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        margin-right: 20px;
+      }
+      .player-title{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        h3{
+          @include font_size($font_medium);
+          @include font_color();
+        }
+        p{
+          @include font_size($font_medium_s);
+          @include font_color();
+        }
+      }
+    }
+    .player-right{
+      display: flex;
+      align-items: center;
+      .play{
+        width: 84px;
+        height: 84px;
+        @include bg_img('../../assets/images/play')
+      }
+      .list{
+        width: 120px;
+        height: 120px;
+        @include bg_img('../../assets/images/list')
+      }
+    }
+  }
+}
+</style>
--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="player">
       <NormalPlayer></NormalPlayer>
+      <MiniPlayer></MiniPlayer>
     </div>
 </template>
 
 <script>
 import NormalPlayer from '../components/Player/NormalPlayer'
+import MiniPlayer from '../components/Player/MiniPlayer'
 export default {
   name: 'Player',
   components: {
-    NormalPlayer
+    NormalPlayer,
+    MiniPlayer
   }
 }
 </script>
