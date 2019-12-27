35cb9a9cc4109324d17092445fce4a21758b4c9d
Jonathan_Lee <lnj@it666.com>
2019-12-27 15:38:10 +0800

    播放界面-播放模式同步

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -3,7 +3,7 @@
       <NormalPlayer :totalTime="totalTime" :currentTime="currentTime"></NormalPlayer>
       <MiniPlayer></MiniPlayer>
       <ListPlayer></ListPlayer>
-      <audio :src="currentSong.url" ref="audio" @timeupdate="timeupdate"></audio>
+      <audio :src="currentSong.url" ref="audio" @timeupdate="timeupdate" @ended="end"></audio>
     </div>
 </template>
 
@@ -11,7 +11,8 @@
 import NormalPlayer from '../components/Player/NormalPlayer'
 import MiniPlayer from '../components/Player/MiniPlayer'
 import ListPlayer from '../components/Player/ListPlayer'
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import mode from '../store/modeType'
 export default {
   name: 'Player',
   components: {
@@ -24,13 +25,33 @@ export default {
       'currentSong',
       'isPlaying',
       'currentIndex',
-      'curTime'
+      'curTime',
+      'modeType',
+      'songs'
     ])
   },
   methods: {
+    ...mapActions([
+      'setCurrentIndex'
+    ]),
     timeupdate (e) {
       // console.log(e.target.currentTime)
       this.currentTime = e.target.currentTime
+    },
+    end () {
+      if (this.modeType === mode.loop) {
+        this.setCurrentIndex(this.currentIndex + 1)
+      } else if (this.modeType === mode.one) {
+        this.$refs.audio.play()
+      } else if (this.modeType === mode.random) {
+        let index = this.getRandomIntInclusive(0, this.songs.length - 1)
+        this.setCurrentIndex(index)
+      }
+    },
+    getRandomIntInclusive (min, max) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min + 1)) + min // 含最大值，含最小值
     }
   },
   watch: {
