c857eb711bf51f8ba38ac2640b092b38a35b618e
Jonathan_Lee <lnj@it666.com>
2020-01-20 19:53:57 +0800

    歌手界面-填充歌手数据

--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -1,27 +1,91 @@
 <template>
   <div class="singer">
-    歌手
+    <ScrollView>
+      <ul class="list-wrapper">
+        <li class="list-group" v-for="(value, index) in list" :key="index">
+          <h2 class="group-title">{{keys[index]}}</h2>
+          <ul>
+            <li class="group-item" v-for="obj in list[index]" :key="obj.id">
+              <img v-lazy="obj.img1v1Url" alt="">
+              <p>{{obj.name}}</p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </ScrollView>
   </div>
 </template>
 
 <script>
-// import { getHotArtists } from '../api/index'
-// import { getLetterArtists } from '../api/index'
 import { getAllArtists } from '../api/index'
+import ScrollView from '../components/ScrollView'
 export default {
   name: 'Singer',
+  components: {
+    ScrollView
+  },
   created () {
     getAllArtists()
-      .then(function (result) {
+      .then((result) => {
         console.log(result)
+        this.keys = result.keys
+        this.list = result.list
       })
       .catch(function (err) {
         console.log(err)
       })
+  },
+  data () {
+    return {
+      keys: [],
+      list: []
+    }
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  @import "../assets/css/variable";
+  @import "../assets/css/mixin";
+.singer{
+  position: fixed;
+  top: 184px;
+  bottom: 0;
+  left: 0;
+  right: 0px;
+  @include bg_sub_color();
+  overflow: hidden;
+  .list-wrapper{
+    /*width: 100%;*/
+    /*height: 100%;*/
+    .list-group{
+      .group-title{
+        @include bg_color();
+        @include font_size($font_medium);
+        color: #fff;
+        padding: 10px 20px;
+        box-sizing: border-box;
+      }
+      .group-item{
+        display: flex;
+        justify-content: flex-start;
+        padding: 10px 20px;
+        border-bottom: 1px solid #ccc;
+        img{
+          width: 100px;
+          height: 100px;
+          border-radius: 50%;
+          overflow: hidden;
+        }
+        p{
+          @include font_size($font_medium);
+          @include font_color();
+          display: flex;
+          align-items: center;
+          margin-left: 20px;
+        }
+      }
+    }
+  }
+}
 </style>
