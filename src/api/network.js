7f2094c0a6ff20f350ea1eac7d733680aeba091e
Jonathan_Lee <lnj@it666.com>
2020-02-03 16:30:25 +0800

    网络指示器优化

--- src/api/network.js
+++ src/api/network.js
@@ -1,10 +1,36 @@
 import axios from 'axios'
+import Vue from 'vue'
 
 // 进行一些全局配置
 // axios.defaults.baseURL = 'http://127.0.0.1:3000/'
 axios.defaults.baseURL = 'http://192.168.3.15:3000/'
 axios.defaults.timeout = 5000
+let count = 0
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  count++
+  Vue.showLoading()
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  Vue.hiddenLoading()
+  return Promise.reject(error)
+})
 
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  count--
+  if (count === 0) {
+    Vue.hiddenLoading()
+  }
+  return response
+}, function (error) {
+  // 对响应错误做点什么
+  Vue.hiddenLoading()
+  return Promise.reject(error)
+})
 // 封装自己的get/post方法
 export default {
   get: function (path = '', data = {}) {
--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import fastclick from 'fastclick'
 import './assets/css/base.scss'
 import VueLazyload from 'vue-lazyload'
 import VConsole from 'vconsole'
+import Loading from './plugin/loading/index'
+// 注意点: 如果想通过use的方式来注册组件, 那么必须先将组件封装成插件
+Vue.use(Loading, {
+  title: '正在加载...'
+})
 Vue.use(VueLazyload, {
   // 可以通过配置loading来设置图片还未加载好之前的占位图片
   loading: require('./assets/images/loading.png')
