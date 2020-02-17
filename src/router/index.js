cff3eaa4df8b0399e9f7b3a7498ace8aa885d6f7
Jonathan_Lee <lnj@it666.com>
2020-02-17 16:56:05 +0800

    解决打包后不能刷新问题

--- src/router/index.js
+++ src/router/index.js
@@ -73,7 +73,13 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  /*
+  注意点: 如果Router中使用的是history模式, 那么打包之后的项目不能刷新, 刷新就会出现404
+  解决方案: 1.Router这两个不要使用history模式, 使用hash模式
+            2.在服务端上面进行一些额外的配置
+  * */
+  // mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
