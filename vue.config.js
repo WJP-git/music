b162e0db3b01490e70c67c47fce1cb09c8cf9ab3
Jonathan_Lee <lnj@it666.com>
2020-02-18 21:03:03 +0800

    NodeJS中document的使用

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
-
 module.exports = {
   /* 部署应用包的基本URL, 不设置可能会出现打包后项目找不到资源问题 */
   publicPath: './',
@@ -38,10 +37,20 @@ module.exports = {
       headless: true,
       onlyProduction: true,
       postProcess: route => {
-        // console.log(route)
+        // 预渲染内容写入之前的额外操作
         let reg = /<meta name="viewport".*user-scalable=no">/gi
         let res = route.html.match(reg)
         route.html = route.html.replace(res[1], '')
+
+        // 1.根据字符串创建一个网页
+        let html = new JSDOM(route.html)
+        // 2.从创建好的网页中拿到document对象
+        let dom = html.window.document
+        // 3.找到对应的元素, 删除对应的元素
+        let loadingEle = dom.querySelector('.container')
+        dom.body.removeChild(loadingEle)
+
+        route.html = html.serialize()
         return route
       }
     }
